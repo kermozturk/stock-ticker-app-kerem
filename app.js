92bc8da404cf1d0ffab3524ddf0249660209d339
kermozturk <krmdnzztrk2005@gmail.com>
2025-12-05 01:02:39 -0500

    Update app.js and package.json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 3000;
 var uri = "mongodb+srv://keremozturk_db_user:krm2005dnz@nosqlpractice.2dd9pek.mongodb.net/Stock?retryWrites=true&w=majority";
 
 
+// home page search form
 app.get("/", (req, res) => {
     res.send(`
         <h1>Stock Ticker App </h1>
@@ -20,9 +21,10 @@ app.get("/", (req, res) => {
         `);
 });
 
+// process the user search
 app.get("/process", async (req, res) =>{
-    var searchVal = req.query.search;
-    var searchType = req.query.type;
+    var searchVal = req.query.search; // user text
+    var searchType = req.query.type; // company or ticker
 
     const client = new MongoClient(uri);
     try{
@@ -30,21 +32,30 @@ app.get("/process", async (req, res) =>{
         var db = client.db("Stock");
         var collection = db.collection("PublicCompanies");
         var query;
+
+        // build the query based on the input from the user
         if(searchType == "ticker"){
             query = {ticker: searchVal};
         }
         else{
             query = {company: searchVal};
         }
+
+        // run query and convert it to array
         var results = await collection.find(query).toArray();
+
+        // print to console
         results.forEach(item => {
             console.log(item.company, item.ticker, item.price);
         });
 
+        // display in web
         let item_list = "";
         results.forEach(item => {
             item_list += `<p>${item.company} - ${item.ticker} - $${item.price}</p>`;
         });
+
+        // send results page
         res.send(`
             <h1>Search Results</h1>
             <p>${results.length} results.</p>
